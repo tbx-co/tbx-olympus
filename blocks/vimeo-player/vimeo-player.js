49eaf80392b2a519f212755dd65975ac1a7f6fdb
Parvin Farahzadeh <parv93farah@gmail.com>
2022-12-19 13:04:38 -0500

    intersection observer

--- blocks/vimeo-player/vimeo-player.js
+++ blocks/vimeo-player/vimeo-player.js
@@ -1,18 +1,23 @@
+import { initIntersectionObserver } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  [...block.children].forEach((div) => {
-    const vimeoId = div.childNodes[1].innerHTML;
-    const videoEmbed = `<div class="vimeo-player-frame">
-                          <iframe src="https://player.vimeo.com/video/${vimeoId}" 
-                                  width="400px" height="400px" 
-                                  frameborder="0"
-                                  allow="autoplay; fullscreen; picture-in-picture"
-                                  title="{video_title}" 
-                                  webkitallowfullscreen 
-                                  mozallowfullscreen 
-                                  allowfullscreen>
-                          </iframe>
-                        </div>`;
-    div.insertAdjacentHTML('afterend', videoEmbed);
-    div.remove();
-  });
+  const embedVideo = () => {
+    [...block.children].forEach((div) => {
+      const vimeoId = div.childNodes[1].innerHTML;
+      const videoEmbed = `<div class="vimeo-player-frame">
+                            <iframe src="https://player.vimeo.com/video/${vimeoId}" 
+                                    width="400px" height="400px" 
+                                    frameborder="0"
+                                    allow="autoplay; fullscreen; picture-in-picture"
+                                    title="{video_title}" 
+                                    webkitallowfullscreen 
+                                    mozallowfullscreen 
+                                    allowfullscreen>
+                            </iframe>
+                          </div>`;
+      div.insertAdjacentHTML('afterend', videoEmbed);
+      div.remove();
+    });
+  };
+  initIntersectionObserver({ sections: block, callback: embedVideo, options: { root: null, rootMargin: '0px', threshold: 0.25 } });
 }
--- blocks/youtube-player/youtube-player.js
+++ blocks/youtube-player/youtube-player.js
@@ -1,14 +1,20 @@
+import { initIntersectionObserver } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  [...block.children].forEach((div) => {
-    const youtubeId = div.childNodes[1].innerHTML;
-    const videoEmbed = `<div class="youtube-player-frame">
-                          <iframe  width="560" height="315"
-                                  src="https://www.youtube.com/embed/${youtubeId}" 
-                                  title="YouTube video player" frameborder="0" allow="accelerometer; 
-                                  autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                  allowfullscreen></iframe>
-                        </div>`;
-    div.insertAdjacentHTML('afterend', videoEmbed);
-    div.remove();
-  });
+  const embedVideo = () => {
+    console.log('hellooooooooooo');
+    [...block.children].forEach((div) => {
+      const youtubeId = div.childNodes[1].innerHTML;
+      const videoEmbed = `<div class="youtube-player-frame">
+                            <iframe  width="560" height="315"
+                                    src="https://www.youtube.com/embed/${youtubeId}" 
+                                    title="YouTube video player" frameborder="0" allow="accelerometer; 
+                                    autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+                            </iframe>
+                          </div>`;
+      div.insertAdjacentHTML('afterend', videoEmbed);
+      div.remove();
+    });
+  };
+  initIntersectionObserver({ sections: block, callback: embedVideo, options: { root: null, rootMargin: '0px', threshold: 0.25 } });
 }
