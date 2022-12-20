0a306ac81276f1d2b6dea10c4d80dce4779c0045
Parvin Farahzadeh <parv93farah@gmail.com>
2022-12-20 12:17:14 -0500

    hero items positioning

---
+++ blocks/hero/hero.js
@@ -0,0 +1,30 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+function optimizeImage(img) {
+  return createOptimizedPicture(
+    img.src,
+    img.alt,
+    true,
+    [{ media: '(min-width: 400px)', width: '2000' }, { width: '750' }],
+  );
+}
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    // images
+    [...row.children].forEach((div) => {
+      if (div.querySelector('picture')) {
+        const image = document.createElement('div');
+        image.className = 'hero-image-frame';
+        div.parentNode.insertBefore(image, div);
+        image.append(div);
+
+        div.querySelectorAll('img')
+          .forEach((img) => img.closest('picture')
+            .replaceWith(optimizeImage(img)));
+      } else {
+        div.className = 'hero-copy';
+      }
+    });
+  });
+}
