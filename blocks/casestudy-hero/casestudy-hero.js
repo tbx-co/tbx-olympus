aba8bc419dd0458a1a98e5ed6caaafcea842681f
Saugat Malla <saugat013@gmail.com>
2023-02-02 17:25:47 -0500

    fix heading class and fadeup

--- blocks/casestudy-hero/casestudy-hero.js
+++ blocks/casestudy-hero/casestudy-hero.js
@@ -1,21 +1,27 @@
 export default function decorate(block) {
-	const picture = block.querySelector('picture');
-	const eyebrow = block.querySelector('p');
-	const heading = block.querySelector('h1,h2,h3');
+  const picture = block.querySelector('picture');
+  const eyebrow = block.querySelector('p');
+  const heading = block.querySelector('h1, h2, h3');
 
-	block.innerHTML = '';
+  block.innerHTML = '';
 
-	const pictureWrapper = document.createElement('div');
-	pictureWrapper.setAttribute('class', 'picture-wrapper');
+  const pictureWrapper = document.createElement('div');
+  pictureWrapper.setAttribute('class', 'picture-wrapper');
 
-	const copyWrapper = document.createElement('div');
-	copyWrapper.setAttribute('class', 'copy-wrapper');
+  const copyWrapper = document.createElement('div');
+  copyWrapper.setAttribute('class', 'copy-wrapper');
 
-	const backdrop = document.createElement('div');
-	backdrop.setAttribute('class', 'casestudy-hero-backdrop');
+  const fadeupWrapper = document.createElement('div');
+  fadeupWrapper.setAttribute('class', 'fadeup');
 
-	pictureWrapper.append(picture);
-	copyWrapper.append(eyebrow, heading);
+  const backdrop = document.createElement('div');
+  backdrop.setAttribute('class', 'casestudy-hero-backdrop');
 
-	block.append(pictureWrapper, backdrop, copyWrapper);
+  heading.setAttribute('class', 'casestudy-hero-heading');
+
+  pictureWrapper.append(picture);
+  fadeupWrapper.append(eyebrow, heading);
+  copyWrapper.append(fadeupWrapper);
+
+  block.append(pictureWrapper, backdrop, copyWrapper);
 }
