8b4c36e577f19aae396e3b6481b26a62752a04d6
snowiewdev <snowiewongdev@gmail.com>
2023-02-03 15:22:20 -0800

    add vimeo play button & vimeo cdn

--- blocks/vimeo-player/vimeo-player.js
+++ blocks/vimeo-player/vimeo-player.js
@@ -3,6 +3,7 @@ import { createTag, replaceAllChildElements } from '../../scripts/helpers.js';
 
 export default function decorate(block) {
   const embedVideo = () => {
+    // add vimeo video player to block
     const vimeoID = block.textContent.trim();
     const videoEmbed = createTag('iframe', {
       src: `https://player.vimeo.com/video/${vimeoID}`,
@@ -13,6 +14,23 @@ export default function decorate(block) {
     const videoEmbedWrapper = createTag('div', { class: 'vimeo-player-frame' }, videoEmbed);
 
     replaceAllChildElements(block, videoEmbedWrapper);
+
+    // toggle visibility of play button based on vimeo player status
+    const vimeoPlayer = document.querySelector('.vimeo-player');
+    const playButton = createTag('button', { class: 'video-player-button' }, '');
+    vimeoPlayer.append(playButton);
+
+    // eslint-disable-next-line no-undef
+    const player = new Vimeo.Player(videoEmbed);
+    player.on('play', () => {
+      playButton.classList.add('is-hidden');
+    });
+    player.on('pause', () => {
+      playButton.classList.remove('is-hidden');
+    });
+    playButton.addEventListener('click', () => {
+      player.play();
+    });
   };
 
   initIntersectionObserver({
