afba0adb35096729f295aacf4a503f99bd1af9d0
snowiewdev <snowiewongdev@gmail.com>
2023-02-03 12:06:01 -0800

    refactor to remove console warnings

--- blocks/vimeo-player/vimeo-player.js
+++ blocks/vimeo-player/vimeo-player.js
@@ -1,23 +1,25 @@
 import { initIntersectionObserver } from '../../scripts/scripts.js';
+import { createTag, replaceAllChildElements } from '../../scripts/helpers.js';
 
 export default function decorate(block) {
   const embedVideo = () => {
-    [...block.children].forEach((div) => {
-      const vimeoId = div.childNodes[1].innerHTML;
-      const videoEmbed = `<div class="vimeo-player-frame">
-                            <iframe src="https://player.vimeo.com/video/${vimeoId}" 
-                                    width="400px" height="400px" 
-                                    frameborder="0"
-                                    allow="autoplay; fullscreen; picture-in-picture"
-                                    title="{video_title}" 
-                                    webkitallowfullscreen 
-                                    mozallowfullscreen 
-                                    allowfullscreen>
-                            </iframe>
-                          </div>`;
-      div.insertAdjacentHTML('afterend', videoEmbed);
-      div.remove();
-    });
+    const vimeoID = block.textContent.trim();
+    const videoEmbed = createTag('iframe', {
+      src: `https://player.vimeo.com/video/${vimeoID}`,
+      title: 'Vimeo video player',
+      frameborder: 0,
+      allow: 'autoplay; picture-in-picture; fullscreen;',
+    }, '');
+    const videoEmbedWrapper = createTag('div', { class: 'vimeo-player-frame' }, videoEmbed);
+
+    replaceAllChildElements(block, videoEmbedWrapper);
   };
-  initIntersectionObserver({ sections: block, callback: embedVideo, options: { root: null, rootMargin: '0px', threshold: 0.25 } });
+
+  initIntersectionObserver({
+    sections: block,
+    callback: embedVideo,
+    options: {
+      root: null, rootMargin: '0px', threshold: 0.25,
+    },
+  });
 }
