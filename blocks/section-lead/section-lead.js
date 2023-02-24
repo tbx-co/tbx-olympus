e403531cf88934ce2c9ba897e254879351b0d954
Paul Solomon <paul@thinkingbox.com>
2023-02-23 21:40:21 -0700

    header statement / section lead work if no icons

--- blocks/section-lead/section-lead.js
+++ blocks/section-lead/section-lead.js
@@ -1,5 +1,15 @@
+import {
+  createTag, replaceAllChildElements,
+} from '../../scripts/helpers.js';
+
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    row.className = 'section-lead__inner-wrapper';
-  });
+  if (block.children.children.length > 1) {
+    [...block.children].forEach((row) => {
+      row.className = 'section-lead__inner-wrapper';
+    });
+  } else {
+    block.children[0].className = 'section-lead__inner-wrapper';
+    const p = createTag('p', { class: '' }, block.children[0].children[0].textContent);
+    replaceAllChildElements(block.children[0], p);
+  }
 }
