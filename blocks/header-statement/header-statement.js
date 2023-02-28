4f82315c432ea5c1cf6fe97095d6db55b3ba9578
Paul Solomon <paul@thinkingbox.com>
2023-02-28 14:05:46 -0700

    header statement
    dynamic image for header statement

--- blocks/header-statement/header-statement.js
+++ blocks/header-statement/header-statement.js
@@ -1,5 +1,12 @@
 export default function decorate(block) {
   const nodeH1 = block.getElementsByTagName('h1')[0];
+
+  const pattern = /\b(img-\S+)\b/;
+  const bgImage = block.classList.toString().match(pattern);
+  if (bgImage) {
+    nodeH1.style.backgroundImage = `url("/blocks/header-statement/${bgImage[0]}.png")`;
+  }
+
   const spanH1 = document.createElement('span');
   spanH1.textContent = nodeH1.textContent;
   nodeH1.innerHTML = null;
