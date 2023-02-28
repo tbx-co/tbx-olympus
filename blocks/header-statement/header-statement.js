846ebcd1b9145570e647175f38283ff1aebe95c7
Paul Solomon <paul@thinkingbox.com>
2023-02-28 14:43:14 -0700

    header statement
    header statement no image

--- blocks/header-statement/header-statement.js
+++ blocks/header-statement/header-statement.js
@@ -4,14 +4,16 @@ export default function decorate(block) {
   const pattern = /\b(img-\S+)\b/;
   const bgImage = block.classList.toString().match(pattern);
   if (bgImage) {
+    const spanH1 = document.createElement('span');
+    spanH1.textContent = nodeH1.textContent;
+    nodeH1.innerHTML = null;
+    nodeH1.appendChild(spanH1);
     nodeH1.style.backgroundImage = `url("/blocks/header-statement/${bgImage[0]}.png")`;
+  } else {
+    nodeH1.style.backgroundImage = null;
   }
 
-  const spanH1 = document.createElement('span');
-  spanH1.textContent = nodeH1.textContent;
-  nodeH1.innerHTML = null;
-  nodeH1.appendChild(spanH1);
-
+  // additional text, not the H1
   if (block.children.length > 1) {
     const nodeP1 = block.children[1];
     nodeP1.classList.add('header-statement__text');
