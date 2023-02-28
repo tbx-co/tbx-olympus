2da06b200ab2c9c677de62edbb00931a7e667437
Paul Solomon <paul@thinkingbox.com>
2023-02-28 11:06:25 -0700

    home, footer
    - show copyright on mobile
    - rework headerstatement to handle another piece of text
    - hero section padding

--- blocks/header-statement/header-statement.js
+++ blocks/header-statement/header-statement.js
@@ -4,4 +4,10 @@ export default function decorate(block) {
   spanH1.textContent = nodeH1.textContent;
   nodeH1.innerHTML = null;
   nodeH1.appendChild(spanH1);
+
+  if (block.children.length > 1) {
+    const nodeP1 = block.children[1];
+    nodeP1.classList.add('header-statement__text');
+    nodeH1.parentNode.insertBefore(nodeP1, nodeH1.nextSibling);
+  }
 }
